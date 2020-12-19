4cb6e775143bb19ae5bcd436db3a279eadbaa162
Navatar <umdomby@yandex.ru>
2020-12-19 13:59:43 +0300

    update

---
+++ src/main/java/com/example/concept/ConceptApplication.java
@@ -0,0 +1,13 @@
+package com.example.concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptApplication.class, args);
+	}
+
+}
